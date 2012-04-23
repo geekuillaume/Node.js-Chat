1c244580671f4e4c302ce39854a3c9e169b5435a
Geekuillaume <geek.uillaume@gmail.com>
2012-04-23 22:00:12 +0200

    Remove time difference between server and client

--- public/script.js
+++ public/script.js
@@ -46,7 +46,7 @@ socket.on('nbUsers', function(msg) {
 	$("#nbUsers").html(msg.nb);
 });
 socket.on('message', function(data) {
-	addMessage(data['message'], data['pseudo'], data['date'], false);
+	addMessage(data['message'], data['pseudo'], new Date().toISOString(), false);
 	console.log(data);
 });
 
