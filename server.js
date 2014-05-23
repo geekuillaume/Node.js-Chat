0796edc4b0caf01d078c78431f41bb0900051b52
emstans <stansburyem@vcu.edu>
2014-05-23 17:07:35 -0400

    Update server.js
    app.configure() has been removed from Express: https://github.com/visionmedia/express/issues/936

--- server.js
+++ server.js
@@ -20,9 +20,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set("view options", { layout: false })
 
-app.configure(function() {
-	app.use(express.static(__dirname + '/public'));
-});
+app.use(express.static(__dirname + '/public'));
 
 // Render and send the main page
 
