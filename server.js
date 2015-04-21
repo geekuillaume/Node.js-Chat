5beca78dcbfbb5969efba566208e7d6622c1ea52
Zisis Tsioumaras <zitsiou@gmail.com>
2015-04-21 22:50:40 +0300

    Updates for socket.io version 1.3 and express 4.12

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/', function(req, res){
 });
 server.listen(appPort);
 // app.listen(appPort);
-console.log("Server listening on port 16558");
+console.log("Server listening on port " + appPort);
 
 // Handle the socket.io connections
 
@@ -41,7 +41,7 @@ io.sockets.on('connection', function (socket) { // First connection
 	socket.on('message', function (data) { // Broadcast the message to all
 		if(pseudoSet(socket))
 		{
-			var transmit = {date : new Date().toISOString(), pseudo : returnPseudo(socket), message : data};
+			var transmit = {date : new Date().toISOString(), pseudo : socket.nickname, message : data};
 			socket.broadcast.emit('message', transmit);
 			console.log("user "+ transmit['pseudo'] +" said \""+data+"\"");
 		}
@@ -49,11 +49,10 @@ io.sockets.on('connection', function (socket) { // First connection
 	socket.on('setPseudo', function (data) { // Assign a name to the user
 		if (pseudoArray.indexOf(data) == -1) // Test if the name is already taken
 		{
-			socket.set('pseudo', data, function(){
-				pseudoArray.push(data);
-				socket.emit('pseudoStatus', 'ok');
-				console.log("user " + data + " connected");
-			});
+			pseudoArray.push(data);
+			socket.nickname = data;
+			socket.emit('pseudoStatus', 'ok');
+			console.log("user " + data + " connected");
 		}
 		else
 		{
@@ -65,10 +64,9 @@ io.sockets.on('connection', function (socket) { // First connection
 		reloadUsers();
 		if (pseudoSet(socket))
 		{
+			console.log("disconnect...");
 			var pseudo;
-			socket.get('pseudo', function(err, name) {
-				pseudo = name;
-			});
+			pseudo = socket.nickname;
 			var index = pseudoArray.indexOf(pseudo);
 			pseudo.slice(index - 1, 1);
 		}
@@ -80,17 +78,7 @@ function reloadUsers() { // Send the count of the users to all
 }
 function pseudoSet(socket) { // Test if the user has a name
 	var test;
-	socket.get('pseudo', function(err, name) {
-		if (name == null ) test = false;
-		else test = true;
-	});
+	if (socket.nickname == null ) test = false;
+	else test = true;
 	return test;
-}
-function returnPseudo(socket) { // Return the name of the user
-	var pseudo;
-	socket.get('pseudo', function(err, name) {
-		if (name == null ) pseudo = false;
-		else pseudo = name;
-	});
-	return pseudo;
 }
